6dafb959b44639e53e2fd9ca8d5082af254f70c3
tomoya <blackie0110@gmail.com>
2020-03-28 14:06:35 +0900

    initual commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  siteMetadata: {
+    title: "playing",
+    description: "sample site for playing around with gatsby",
+    author: 'STomoya',
+    copyright_year: 2020
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-transformer-sharp`
+    },
+  ]
+}
